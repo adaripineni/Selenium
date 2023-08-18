c0122dcbbcea376ac335a7390a2f42e2a7871abc
Amulya D <amulya.daripineni@gmail.com>
2023-08-18 15:52:51 -0700

    changed wait

--- src/main/java/com/github/amulya/SeleniumAutomation/DropdownTest.java
+++ src/main/java/com/github/amulya/SeleniumAutomation/DropdownTest.java
@@ -48,7 +48,7 @@ public class DropdownTest {
 		d.manage().window().maximize();
 		d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);		
 		d.get("https://www.globalsqa.com/demo-site/select-dropdown-menu/");
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 	} 
 	
 	@Test
