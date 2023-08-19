ec9771757c3ae870bee2f9569b54422cc4043469
Amulya D <amulya.daripineni@gmail.com>
2023-08-18 19:14:52 -0700

    changed sleep

--- src/main/java/com/github/amulya/SeleniumAutomation/DropdownTest.java
+++ src/main/java/com/github/amulya/SeleniumAutomation/DropdownTest.java
@@ -61,7 +61,7 @@ public class DropdownTest {
 		for( WebElement option:options) {
 			if(option.getText().startsWith("B")){
 				menu.selectByVisibleText(option.getText());
-				Thread.sleep(4000);
+				Thread.sleep(5000);
 			}
 		}
 //		menu.selectByIndex(options.size()-1);
